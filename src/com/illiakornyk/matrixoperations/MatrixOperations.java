733e77c8e6f5c4735146ca6770d8ca589d7c627c
Illia Kornyk <krekerbrow@gmail.com>
2024-09-27 22:07:00 +0300

    feat: bitwise xor method

--- src/com/illiakornyk/matrixoperations/MatrixOperations.java
+++ src/com/illiakornyk/matrixoperations/MatrixOperations.java
@@ -21,17 +21,46 @@ public class MatrixOperations {
         int C7 = studentId % 7;
         int C11 = studentId % 11;
 
-
-        System.out.println("C5 (studentId % 5): " + C5);
-        System.out.println("C7 (studentId % 7): " + C7);
-        System.out.println("C11 (studentId % 11): " + C11);
+        performBitwiseXor();
     }
 
     public static void main(String[] args) {
         MatrixOperations matrixOps = new MatrixOperations(8);
         System.out.println("Order number: " + matrixOps.getStudentId());
 
-
         matrixOps.calculateCValues();
+    }
+
+
+    private void performBitwiseXor() {
+        int[][] A = {
+                {1, 2},
+                {3, 4}
+        };
+
+        int[][] B = {
+                {5, 6},
+                {7, 8}
+        };
+
+        int[][] C = new int[A.length][A[0].length];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                C[i][j] = A[i][j] ^ B[i][j];
+            }
+        }
+
+        System.out.println("Resulting matrix C (A XOR B):");
+        printMatrix(C);
+    }
+
+    private void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
     }
 }
