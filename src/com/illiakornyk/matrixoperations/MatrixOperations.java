9a55e97cba878d288505912ab114b972c35dca8b
Illia Kornyk <krekerbrow@gmail.com>
2024-09-29 14:33:41 +0300

    feat: calculate row average

--- src/com/illiakornyk/matrixoperations/MatrixOperations.java
+++ src/com/illiakornyk/matrixoperations/MatrixOperations.java
@@ -3,9 +3,25 @@ package com.illiakornyk.matrixoperations;
 public class MatrixOperations {
 
     private int studentId;
+    private byte[][] A;
+    private byte[][] B;
+    private byte[][] C;
+
 
     public MatrixOperations(int studentId) {
         this.studentId = studentId;
+
+        A = new byte[][] {
+                {1, 2},
+                {3, 4}
+        };
+
+        B = new byte[][] {
+                {5, 6},
+                {7, 8}
+        };
+
+        C = new byte[A.length][A[0].length];
     }
 
     public int getStudentId() {
@@ -25,7 +41,13 @@ public class MatrixOperations {
         System.out.println("C7 (studentId % 7): " + C7);
         System.out.println("C11 (studentId % 11): " + C11);
 
-        performBitwiseXor();
+        try {
+            performBitwiseXor();
+
+            calculateRowAverages();
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
@@ -35,19 +57,10 @@ public class MatrixOperations {
         matrixOps.calculateCValues();
     }
 
-
-    private void performBitwiseXor() {
-        byte[][] A = {
-                {1, 2},
-                {3, 4}
-        };
-
-        byte[][] B = {
-                {5, 6},
-                {7, 8}
-        };
-
-        byte[][] C = new byte[A.length][A[0].length];
+    private void performBitwiseXor() throws Exception {
+        if (A.length != B.length || A[0].length != B[0].length) {
+            throw new Exception("Matrices A and B must have the same dimensions");
+        }
 
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[i].length; j++) {
@@ -57,6 +70,18 @@ public class MatrixOperations {
 
         System.out.println("Resulting matrix C (A XOR B):");
         printMatrix(C);
+    }
+
+    private void calculateRowAverages() {
+        System.out.println("Average value for each row:");
+        for (int i = 0; i < C.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < C[i].length; j++) {
+                sum += C[i][j];
+            }
+            double average = (double) sum / C[i].length;
+            System.out.println("Row " + (i + 1) + " average: " + average);
+        }
     }
 
     private void printMatrix(byte[][] matrix) {
