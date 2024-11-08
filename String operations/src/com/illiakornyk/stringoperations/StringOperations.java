4f2d3555932deca64dcea700c24c5533699891f2
Illia Kornyk <forest.bukson@gmail.com>
2024-11-08 08:51:50 +0200

    feat: sort words by letter frequency

--- String operations/src/com/illiakornyk/stringoperations/StringOperations.java
+++ String operations/src/com/illiakornyk/stringoperations/StringOperations.java
@@ -21,6 +21,26 @@ public class StringOperations {
         this.studentId = studentId;
     }
 
+
+    private void sortWordsByLetterFrequency(char targetLetter) {
+        String[] words = sfText.toString().split("\\s+");
+        Map<String, Integer> wordFrequencyMap = new HashMap<>();
+
+        for (String word : words) {
+            int frequency = countOccurrences(word, targetLetter);
+            wordFrequencyMap.put(word, frequency);
+        }
+
+        List<String> sortedWords = new ArrayList<>(wordFrequencyMap.keySet());
+        sortedWords.sort(Comparator.comparingInt(wordFrequencyMap::get));
+
+        System.out.println("Words sorted by frequency of letter '" + targetLetter + "':");
+        for (String word : sortedWords) {
+            System.out.println(word + " (Frequency: " + wordFrequencyMap.get(word) + ")");
+        }
+    }
+
+
     private int countOccurrences(String word, char targetLetter) {
         int count = 0;
         for (char c : word.toCharArray()) {
