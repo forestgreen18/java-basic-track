c73cfcbb2dcec0ae20a3362746c0c52c288ea123
Illia Kornyk <forest.bukson@gmail.com>
2024-11-09 08:55:10 +0200

    feat: equals method

--- Classes/src/com/illiakornyk/student/Student.java
+++ Classes/src/com/illiakornyk/student/Student.java
@@ -1,4 +1,5 @@
 package com.illiakornyk.student;
+import java.util.Objects;
 
 public class Student {
     private final String name;
@@ -14,4 +15,38 @@ public class Student {
         this.gpa = gpa;
         this.major = major;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                studentId == student.studentId &&
+                Double.compare(student.gpa, gpa) == 0 &&
+                Objects.equals(name, student.name) &&
+                Objects.equals(major, student.major);
+    }
+
+
 }
