1441978ff233325b1fcf44de5271a125c2e0d713
Illia Kornyk <forest.bukson@gmail.com>
2024-11-09 19:35:34 +0200

    fix: pass values into class constructor

--- String operations/src/Main.java
+++ String operations/src/Main.java
@@ -2,7 +2,12 @@ import com.illiakornyk.stringoperations.StringOperations;
 
 public class Main {
     public static void main(String[] args) {
-        StringOperations stringOps = new StringOperations();
+        int studentId = 8;
+        StringBuffer textBuffer = new StringBuffer("Example text with various words to sort by frequency.");
+        char targetLetter = 'e';
+
+        StringOperations stringOps = new StringOperations(studentId, textBuffer, targetLetter);
+
         stringOps.performTextOperation();
     }
 }
