78e1bd96b0c59ce7cbd71fce35bf4718c20412fe
Illia Kornyk <forest.bukson@gmail.com>
2024-12-09 00:42:15 +0200

    Project setup

---
+++ Classes relation/src/com/illiakornyk/textstructure/Letter.java
@@ -0,0 +1,18 @@
+package com.illiakornyk.textstructure;
+
+public class Letter {
+    private char character;
+
+    public Letter(char character) {
+        this.character = character;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(character);
+    }
+}
