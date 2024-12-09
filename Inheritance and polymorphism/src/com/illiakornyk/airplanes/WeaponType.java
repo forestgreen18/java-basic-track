6f39e2205432272e450302ff86b27ee7fcb553dd
Illia Kornyk <forest.bukson@gmail.com>
2024-12-09 21:50:53 +0200

    refactor: run formatter

--- Inheritance and polymorphism/src/com/illiakornyk/airplanes/WeaponType.java
+++ Inheritance and polymorphism/src/com/illiakornyk/airplanes/WeaponType.java
@@ -1,9 +1,9 @@
 package com.illiakornyk.airplanes;
 
 public enum WeaponType {
-    MISSILES,
-    BOMBS,
-    GUNS,
-    LASERS,
-    ELECTRONIC_WARFARE
+  MISSILES,
+  BOMBS,
+  GUNS,
+  LASERS,
+  ELECTRONIC_WARFARE
 }
